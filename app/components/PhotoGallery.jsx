52a8ad6ce289b6c46758a9c272ffb9fd6905320a
Gülşah Küçükkoç <gulsah.kucukkoc@gmail.com>
2025-11-13 22:00:50 +0300

    add alt property

--- app/components/PhotoGallery.jsx
+++ app/components/PhotoGallery.jsx
@@ -8,7 +8,7 @@ export default function PhotoGallery() {
         alt=""
         className="w-1/3 h-80"
       />
-      <img src="/images/ödev3.jpg" alt="" className="w-1/3 h-80" />
+      <img src="/images/ödev3.jpg" alt="cghgj" className="w-1/3 h-80" />
       <img
         src="https://images.pexels.com/photos/31875436/pexels-photo-31875436.jpeg?_gl=1*wyc2ss*_ga*NTc2NTk4NTMuMTc2MTIyODc1NA..*_ga_8JE65Q40S6*czE3NjMwNTQ5MzckbzQkZzEkdDE3NjMwNTU3MzUkajUzJGwwJGgw"
         alt=""
